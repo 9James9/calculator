e66732c97f1420b2983e1d1b78121e33d5f0a09d
James Driscoll <thejamesdriscoll@gmail.com>
2021-08-22 15:47:37 -0500

    added event listeners to the rest of the operands

--- script.js
+++ script.js
@@ -37,21 +37,20 @@ display.textContent = 0
 function displayThis (num) {
     if (typeof operand1 == 'undefined') {
         operand1 = num
-        console.log(operand1)
+        console.log(`operand1: ${operand1}`)
         return display.textContent = operand1
     } else if (typeof operand1 !== 'undefined') {
         operand2 = num
-        console.log(operand2)
+        console.log(`operand2: ${operand2}`)
         return display.textContent = operand2
-    }
+    } //else if operand1 and operand2 !== undefined, run operate() and reset operand1 and 2's values after displaying results
 }
 
 
 
 let zeroBtn = document.querySelector('#zero')
 zeroBtn.addEventListener('click', () => {
-    operand1 = 0
-    display.innerHTML = `${operand1}`
+    displayThis(0)
 })
 let threeBtn = document.querySelector('#three')
 threeBtn.addEventListener('click', () => {
@@ -61,6 +60,41 @@ threeBtn.addEventListener('click', () => {
 let twoBtn = document.querySelector('#two')
 twoBtn.addEventListener('click', () => {
     displayThis(2)
+})
+
+let oneBtn = document.querySelector('#one')
+oneBtn.addEventListener('click', () => {
+    displayThis(1)
+})
+
+let sixBtn = document.querySelector('#six')
+sixBtn.addEventListener('click', () => {
+    displayThis(6)
+})
+
+let fiveBtn = document.querySelector('#five')
+fiveBtn.addEventListener('click', () => {
+    displayThis(5)
+})
+
+let fourBtn = document.querySelector('#four')
+fourBtn.addEventListener('click', () => {
+    displayThis(4)
+})
+
+let nineBtn = document.querySelector('#nine')
+nineBtn.addEventListener('click', () => {
+    displayThis(9)
+})
+
+let eightBtn = document.querySelector('#eight')
+eightBtn.addEventListener('click', () => {
+    displayThis(8)
+})
+
+let sevenBtn = document.querySelector('#seven')
+sevenBtn.addEventListener('click', () => {
+    displayThis(7)
 })
   //pressing operand causes its value to be saved as operator and displayed on screen, but the code does not run until there is a second operand
   //pressing another number causes its value to be saved as operand2 and displayed on screen
