beb4894cf50e1b4a793b8a35f631140742291dbb
James Driscoll <thejamesdriscoll@gmail.com>
2021-08-23 10:20:17 -0500

    added code to subtract function

--- script.js
+++ script.js
@@ -1,21 +1,22 @@
-const operate = function(currentNumber,operator,operand2) {
-if (operator = 'add') {
-    let answer = add(currentNumber,operand2)
-    return answer
-} else if (operator = 'subtract') {
-    let answer = subtract(operand1,operand2)
-    return answer
-} else if (operator = 'multiply') {
-    let answer = multiply(operand1,operand2)
-    return answer
+const operate = function (operator) {
+    if (operator == 'add') {
+        let answer = add()
+        return answer
+    } else if (operator == 'subtract') {
+        let answer = subtract()
+        display.textContent = `${answer}`
+        return answer
+    } else if (operator = 'multiply') {
+        let answer = multiply(operand1, operand2)
+        return answer
+    }
 }
-}
-const add = (currentNumber,nextNumber) => {
-    
 
+const add = () => {
+if (operator == 'add') {
     let finalInput = (display.textContent)
     let newInput = finalInput.split(' + ')
-    
+
     console.log(newInput)
     let firstNumber = parseInt(newInput[0])
     let secondNumber = parseInt(newInput[1])
@@ -25,13 +26,24 @@ const add = (currentNumber,nextNumber) => {
     display.textContent = `${answer}`
     return answer
 }
-const subtract = (operand1,operand2) => {
-    return operand1 - operand2
 }
-const multiply = (operand1,operand2) => {
+const subtract = () => {
+    if (operator == 'subtract') {
+    let finalInputSubtract = display.textContent
+    let newInputSubtract = finalInputSubtract.split(' - ')
+    let firstNumberSubtract = parseInt(newInputSubtract[0])
+    let secondNumberSubtract = parseInt(newInputSubtract[1])
+    console.log(`firstNumberSubtract = ${firstNumberSubtract}`)
+    console.log(`secondNumberSubtract = ${secondNumberSubtract}`)
+    let answer = firstNumberSubtract - secondNumberSubtract
+    display.textContent = `${answer}`
+    return answer
+    }
+}
+const multiply = (operand1, operand2) => {
     return operand1 * operand2
 }
-const divide = (operand1,operand2) => {
+const divide = (operand1, operand2) => {
     return operand1 / operand2
 }
 const squareroot = (operand1) => { //will only work with 1 operand so I will need to modify the operate function to use this
@@ -39,77 +51,62 @@ const squareroot = (operand1) => { //will only work with 1 operand so I will nee
 }
 //////////////Operate function ^^^^
 
-//pressing a number causes its value to be saved as operand1 and displayed on screen
-//let operand1 = 0
 
 let display = document.querySelector('#display')
-//display.textContent = 0
 
-//if operand1 is already defined, then next button will define operand2 ****I can change this one I add the operators because I can use an operator event listener to switch when to start recording value from operand1 to operand2. Pressing an operator will store operand 1's value and then begin recording the value for operand2
-/*
-function displayThis (num) { 
-    if (typeof operand1 == 'undefined' || operand1 == '') {
-        operand1 = num
-        console.log(`operand1: ${operand1}`)
-        return display.textContent = operand1
-    } else if (typeof operand1 !== 'undefined' || operand1 !== '' ||operand1 == 0) {
-        operand2 = num
-        console.log(`operand2: ${operand2}`)
-        return display.textContent = operand2
-    } //else if operand1 and operand2 !== undefined, run operate() and reset operand1 and 2's values after displaying results
-}
-*/
-function displayThis (num) {
-    let nextNumber = []
+function displayThis(num) {
+    
     let operator
     if (typeof operator == 'undefined') {
-        let operator = 'off'
-    } else {let operator = 'off'}
-     if (operator == 'off' || 'undefined') {
-         let currentNumber = [
-             
-         ]
-         currentNumber.push(num)
-         console.log(`currentNumber = ${currentNumber = currentNumber}`)
-         
-         display.textContent += currentNumber 
-         return
-     } else if (operator !== 'off') {
-         firstNumber.push(parseInt(display.textContent))
-        
+        operator = 'off'
+    } else {
+        operator = 'off'
+    }
+    if (operator == 'off' || 'undefined') {
+        let currentNumber = [
+
+        ]
+        currentNumber.push(num)
+        console.log(`currentNumber = ${currentNumber = currentNumber}`)
+
+        display.textContent += currentNumber
+        return
+    } else if (operator !== 'off') {
+        firstNumber.push(parseInt(display.textContent))
+
         nextNumber.push(num)
         console.log(nextNumber = `nextNumber = ${nextNumber}`)
-         return
-         
-     }
-     
-     let firstNumber = []
-     /*
-   if (typeof operand1 == 'undefined' && typeof operand2 == 'undefined') {
-       operand1 = num
-       console.log(operand1)
-       display.textContent = operand1
-       
-   } else if (typeof operand1 !== 'undefined' && typeof operand2 == 'undefined' ) {
-       operand1 += num.toString()
-       display.textContent = operand1
-       console.log(operand1)
-   }
-   if (typeof operator !== 'undefined') {
-       
-       if (typeof operand2 == 'undefined') {
-           operand2 = num
-           display.textContent = operand2
+        return
+
+    }
+
+    let firstNumber = []
+    /*
+       if (typeof operand1 == 'undefined' && typeof operand2 == 'undefined') {
+           operand1 = num
+           console.log(operand1)
+           display.textContent = operand1
            
-       } else if (typeof operand2 !== 'undefined') {
-           operand2 += num.toString()
-           display.textContent = operand2
-           
+       } else if (typeof operand1 !== 'undefined' && typeof operand2 == 'undefined' ) {
+           operand1 += num.toString()
+           display.textContent = operand1
+           console.log(operand1)
        }
-       return numberOperand1 = parseInt(operand1)
-   }
-}
-*/
+       if (typeof operator !== 'undefined') {
+           
+           if (typeof operand2 == 'undefined') {
+               operand2 = num
+               display.textContent = operand2
+               
+           } else if (typeof operand2 !== 'undefined') {
+               operand2 += num.toString()
+               display.textContent = operand2
+               
+           }
+           return numberOperand1 = parseInt(operand1)
+       }
+    }
+    */
 }
 //operand buttons
 let zeroBtn = document.querySelector('#zero')
@@ -175,41 +172,22 @@ addBtn.addEventListener('click', () => {
     operator = "add"
     firstNumber.push(display.textContent)
     return display.textContent = `${firstNumber} + `
-    
+
 })
 let equalsBtn = document.querySelector('#equals')
 equalsBtn.addEventListener('click', () => {
-    operate(operator = 'add')
-    //display.textContent = `answer!`
-    return
+    operate(operator)  
 })
-/*
-let equalsBtn = document.querySelector('#equals')
-equalsBtn.addEventListener('click', () => {
-    
-    if (typeof currentNumber == 'undefined' || typeof operand2 == 'undefined' || typeof operator == 'undefined') {display.textContent = "Error!"
-    } else if (typeof operand1 !== 'undefined' && typeof operand2 !== 'undefined' || typeof operator !== 'undefined') {
-        
-        //let numberOperand1 = parseInt(operand1)
-        let numberOperand2 = parseInt(operand2)
-        let numberOperand1 = parseInt(operand1)
-        console.log(typeof operand1)
-        console.log(typeof operand2)
-        console.log(typeof numberOperand1)
-        console.log(typeof numberOperand2)
-        display.textContent = operate(currentNumber,operator,numberOperand2)
-        
-        operand1 = ''
-        operand2 = ''
-        return 
-        
-    }
+let subtractBtn = document.querySelector('#subtract')
+subtractBtn.addEventListener('click', () => {
+    let firstNumber = []
+    firstNumber.push(display.textContent)
+
+    display.textContent = `${firstNumber} - `
+    return operator = 'subtract'
 })
 
-*/
 
-
-  //pressing operand causes its value to be saved as operator and displayed on screen, but the code does not run until there is a second operand
-  //pressing another number causes its value to be saved as operand2 and displayed on screen
-    //operate runs when it has all 3 parameters && when equals is pressed
-
+//pressing operand causes its value to be saved as operator and displayed on screen, but the code does not run until there is a second operand
+//pressing another number causes its value to be saved as operand2 and displayed on screen
+//operate runs when it has all 3 parameters && when equals is pressed
